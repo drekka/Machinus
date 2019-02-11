71132a8acdffd1b17df64e8da3fc5b25849d16c5
Derek Clarkson <d4rkf1br3@gmail.com>
2019-02-11 21:41:42 +1100

    Initial commit

---
+++ Machinus/Machinus.h
@@ -0,0 +1,19 @@
+//
+//  Machinus.h
+//  Machinus
+//
+//  Created by Derek Clarkson on 11/2/19.
+//  Copyright © 2019 Derek Clarkson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Machinus.
+FOUNDATION_EXPORT double MachinusVersionNumber;
+
+//! Project version string for Machinus.
+FOUNDATION_EXPORT const unsigned char MachinusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Machinus/PublicHeader.h>
+
+
---
+++ MachinusTests/MachinusTests.swift
@@ -0,0 +1,34 @@
+//
+//  MachinusTests.swift
+//  MachinusTests
+//
+//  Created by Derek Clarkson on 11/2/19.
+//  Copyright © 2019 Derek Clarkson. All rights reserved.
+//
+
+import XCTest
+@testable import Machinus
+
+class MachinusTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
